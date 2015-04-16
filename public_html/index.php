4bcf6313852aff63f0c2f9f0af8b6e9cf31d0465
fergburglerr <ngfnbf@mail.missouri.edu>
2015-04-15 19:27:04 -0500

    initialized css

--- public_html/index.php
+++ public_html/index.php
@@ -4,6 +4,7 @@
 	<title>UUCC Carpool WebApp</title>
 	<script src="js/jquery-1.11.2.min.js"></script>
 	<script src="js/UU.js"></script>
+	<link rel="stylesheet" type="text/css" href="css/UU.css">
 
 </head>
 <body>
