b75d607abf0dedf4406f0684c56ad9b1588b5ebf
fergburglerr <ngfnbf@mail.missouri.edu>
2015-04-16 17:46:11 -0500

    Added jquery UI files

--- public_html/index.php
+++ public_html/index.php
@@ -2,13 +2,15 @@
 <html>
 <head>
 	<title>UUCC Carpool WebApp</title>
-	<script src="js/jquery-1.11.2.min.js"></script>
-	<script src="js/UU.js"></script>
+	<link rel="stylesheet" type="text/css" href="css/jquery-ui.min.css">
 	<link rel="stylesheet" type="text/css" href="css/UU.css">
-
+	<script src="js/jquery-1.11.2.min.js"></script>
+	<script src="js/jquery-ui.min.js"></script>
+	<script src="js/UU.js"></script>
 </head>
 <body>
-
+	<div id="fg">
+	</div>
 	<?php
 
 	?>
