dc5b1503abbc9e7ef645df472389411c6703a36f
Thomas Scully <thomas1374@yahoo.com>
2015-04-16 19:24:04 -0500

    Added PHP for announcements

---
+++ public_html/sql/announcements.php
@@ -0,0 +1,49 @@
+<?PHP
+
+include("db_info.php");//including my database connection
+$conn = new mysqli($host, $user, $pass, $db);
+if(!$conn->connect_error){
+	echo"<p>Connection to database failed...</p>";
+}
+
+
+# Function to add a new announcement 
+if $_POST['action']=='add'{
+
+	$message = htmlspecialchars($_POST['text']);
+
+	if (strlen($message) < 2047) {
+		$query = 'INSERT INTO Announcement (aid, content, aDate) VALUES (?, ?, ?);';
+		$stmt = $conn->prepare($query);
+		$stmt->bind_param("iss", default, $message, date("Y-m-d H:i:s"));
+		$stmt->execute();
+
+		echo "New announcement created successfully!!!";
+		$stmt->close();
+	}
+	else {
+		die("Content of message is too long!!!");
+	}
+
+}
+
+# Function to edit an announcement
+else if $_POST['action']=='edit'{
+
+}
+
+# Function to remove an announcement
+else if $_POST['action']=='remove'{
+
+}
+
+#Retrieves all announcements 
+else if $_POST['action']=='get' {
+
+}
+
+else {
+	echo"There was an error with the POST request, please contact Joe since it is probably his fault";
+}
+mysqli_close($conn);
+?>
