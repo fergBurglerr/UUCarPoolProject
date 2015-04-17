a08b32496689a0f4b07fb5efbee9f41862c5b3cc
JET005 <ethanwtsn.watson5@gmail.com>
2015-04-17 10:03:58 -0500

    finished edit event, delete event, and get event

--- public_html/event.php
+++ public_html/event.php
@@ -25,20 +25,55 @@ if(strcmp($_POST['action'],"insert")==0){
 	$eventType = $_POST['test'];
 
 	$result->execute();
+	
 	printf("%d Row inserted.\n", $result->affected_rows);
 	$result->close();
 }
 //edit
-if(strcmp($_POST['action'],"insert")==0){
+if(strcmp($_POST['action'],"edit")==0){
+	echo "update";
+	$result = $conn->prepare("UPDATE Event SET eventName=? WHERE eid=?");
+	echo $result->prepare_error;
+	$result->bind_param('si',$edit,$eventid);
 
+	$option=$_POST['option'];
+	$edit=$_POST['edit'];
+	$eventid=$_POST['eid'];
+	$result->execute();
+	printf("%d Row updated.\n", $result->affected_rows);
+
+	$result->close();
 }
 
 
 
 //remove
+if(strcmp($_POST['action'], "delete")==0){
+	$result = $conn->prepare("DELETE FROM Event WHERE eid=?");
+	$result->bind_param('i',$eventid);
 
+	$eventid=$_POST['eid'];
+	$result->execute();
+	printf("%d Row Deleted.\n", $result->affected_rows);
+
+	$result->close();
+}
 
 //getevent 
+//if(strcmp($_POST['action'],"get")==0){
+	$result = $conn->prepare("SELECT eid,eventName,startTime,endTime,description,eventType FROM Event ORDER BY startTime limit 10 offset ?");
+	$result->bind_param('i',$offset);
+
+	$offset=0;
+	$result->execute();
+	$result->bind_result($eid,$name,$start,$end,$description,$type);
+	while ($result->fetch()) {
+        printf ("id: %s name: %s start time: %s end time: %s description: %s type: %s\n <br>", $eid, $name,$start,$end,$description,$type);
+    }
+//}
+
+
+$result->close();
 
 
 $conn->close();
