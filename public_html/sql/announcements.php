8e36d21b024a1277c60d90b3bc197c7f5398d455
Thomas Scully <tps9tb@mail.missouri.edu>
2015-04-18 13:58:30 -0500

    Made changed to announcements

--- public_html/sql/announcements.php
+++ public_html/sql/announcements.php
@@ -7,15 +7,16 @@ if($conn->connect_error){
 }
 
 
-# Function to add a new announcement 
-if ($_POST['action']=='add'){
+##### Function to add a new announcement 
+#if ($_POST['action']=='add'){
 
-	$message = htmlspecialchars($_POST['text']);
+	#$message = htmlspecialchars($_POST['text']);
+	$message = htmlspecialchars("This worked fine!!!");
 
 	if (strlen($message) < 2047) {
-		$query = 'INSERT INTO Announcement (aid, content, aDate) VALUES (default, ?, ?);';
+		$query = 'INSERT INTO Announcement (content, aDate) VALUES (?, ?);';
 		$stmt = $conn->prepare($query);
-		$stmt->bind_param("iss", $message, date("Y-m-d H:i:s"));
+		$stmt->bind_param("ss", $message, date("Y-m-d H:i:s"));
 		$stmt->execute();
 
 		echo "New announcement created successfully!!!";
@@ -25,25 +26,25 @@ if ($_POST['action']=='add'){
 		die("Content of message is too long!!!");
 	}
 
-}
+#}
 
-# Function to edit an announcement
-else if ($_POST['action']=='edit'){
+##### Function to edit an announcement
+#else if ($_POST['action']=='edit'){
 
-}
+#}
 
-# Function to remove an announcement
-else if ($_POST['action']=='remove'){
+##### Function to remove an announcement
+#else if ($_POST['action']=='remove'){
 
-}
+#}
 
-#Retrieves all announcements 
-else if ($_POST['action']=='get'){
+##### Retrieves all announcements 
+#else if ($_POST['action']=='get'){
 
-}
+#}
 
-else {
-	echo"There was an error with the POST request, please contact Joe since it is probably his fault";
-}
+#else {
+	#echo"There was an error with the POST request, please contact Joe since it is probably his fault";
+#}
 mysqli_close($conn);
 ?>
