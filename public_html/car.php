386179de3f3fb588ef1fea799e4a4042c8c6c261
JET005 <ethanwtsn.watson5@gmail.com>
2015-04-20 18:44:27 -0500

    Switched from _GET to _POST

--- public_html/car.php
+++ public_html/car.php
@@ -1,80 +1,84 @@
-<?php 
-include("sql/dbinfo.php");
+<?php
+	session_start();
+	###PHP functions to add, modify, delete, and add assign cars to people
+	include("sql/dbinfo.php");
 
-//$conn = mysql_connect($host, $user, $pass);
-// if (!$conn) {
-//     die('Could not connect: ' . mysql_error());
-// }
+	###Create connection
+	$conn = new mysqli($host, $user, $pass, $db);
+	###Check connection
+	if ($conn->connect_error) {
+	    die("Connection failed: " . $conn->connect_error);
+	} 
 
-// Create connection
-$conn = new mysqli($host, $user, $pass, $db);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
-//insert
-if(strcmp($_POST['action'],"insert")==0){
-	$result = $conn->prepare("INSERT INTO Event (eventName,startTime,endTime,description,eventType) 
-	VALUES (?,?,?,?,?)");
-	$result->bind_param('sssss',$eventName,$startTime,$endTime,$description,$eventType);
+	###Insert function for Cars 
+	if (strcmp($_POST['action'],'add')==0){
+		$result = $conn->prepare("INSERT INTO Car (openSeats,make,model,color,license_num) 
+		VALUES (?,?,?,?,?)");
+		
+		$openSeats=4;
+		$make="Chevy";
+		$model="Cavalier";
+		$color="Green";
+		$license_num="ge8 3ds";
+		$result->bind_param('issss',$openSeats,$make,$model,$color,$license_num);
 
-	$eventName=$_POST['eventName'];
-	$startTime=$_POST['startTime'];
-	$endTime = $_POST['endTime'];
-	$description = $_POST['description'];
-	$eventType = $_POST['test'];
+		if ($result->execute()) {
+			echo "$result->insert_id";
+			$cid = $result->insert_id;
+			$result2 = $conn->prepare("INSERT INTO person_has_car (pid, cid) VALUES (?,?);");
+			$result2->bind_param('ii', $pid, $cid);
+			$pid=$_SESSION['pid'];
 
-	$result->execute();
-	
-	printf("%d Row inserted.\n", $result->affected_rows);
-	$result->close();
-}
-//edit
-if(strcmp($_POST['action'],"edit")==0){
-	echo "update";
-	$result = $conn->prepare("UPDATE Event SET eventName=? WHERE eid=?");
-	echo $result->prepare_error;
-	$result->bind_param('si',$edit,$eventid);
+			if ($result2->execute()) {
+				echo "New Car created successfully!!!";
+			}
+			$result2->close();
+		}
+		else {
+		echo "Something went wrong with insert function";
+		}
+		$result->close();
+	}
 
-	$option=$_POST['option'];
-	$edit=$_POST['edit'];
-	$eventid=$_POST['eid'];
-	$result->execute();
-	printf("%d Row updated.\n", $result->affected_rows);
+	###Remove function for Cars
+	if (strcmp($_POST['action'],'remove')==0){
+		$result = $conn->prepare("DELETE FROM Car WHERE cid=?");
+		$result->bind_param('i',$carid);
 
-	$result->close();
-}
+		$carid= 1;//$_POST['cid'];
+		if($result->execute())
+			printf("%d Row Deleted.\n", $result->affected_rows);
+		
+		$result->close();
+	}
 
+	###Edit function for Cars
+	###Change number of open seats
+	if (strcmp($_POST['action'],'edit')==0){
+		echo "update";
+		$result = $conn->prepare("UPDATE Car SET openSeats=? WHERE cid=?");
+		echo $result->prepare_error;
+		$result->bind_param('ii',$openSeats,$carid);
 
+		$openSeats=$_POST['seats'];
+		$carid=$_POST['cid'];
+		$result->execute();
+		printf("%d Row updated.\n", $result->affected_rows);
 
-//remove
-if(strcmp($_POST['action'], "delete")==0){
-	$result = $conn->prepare("DELETE FROM Event WHERE eid=?");
-	$result->bind_param('i',$eventid);
+		$result->close();
+	}
 
-	$eventid=$_POST['eid'];
-	if($result->execute())
-		printf("%d Row Deleted.\n", $result->affected_rows);
-	
-	$result->close();
-}
+	if (strcmp($_POST['action'],'get')==0) {
+		$result = $conn->prepare("SELECT color, make, model, license_num, openSeats FROM Car;");
 
-//getevent 
-if(strcmp($_POST['action'],"get")==0){
-	$result = $conn->prepare("SELECT eid,eventName,startTime,endTime,description,eventType FROM Event ORDER BY startTime limit 10 offset ?");
-	$result->bind_param('i',$offset);
+		#$offset=0;
+		$result->execute();
+		$result->bind_result($color, $make, $model, $license_num, $openSeats);
+		while ($result->fetch()) {
+	        printf ("color: %s make: %s model: %s license_num: %s openSeats: %s\n <br>", $color, $make, $model, $license_num, $openSeats);
+	    }
+		$result->close();
+	}
 
-	$offset=0;
-	$result->execute();
-	$result->bind_result($eid,$name,$start,$end,$description,$type);
-	while ($result->fetch()) {
-        printf ("id: %s name: %s start time: %s end time: %s description: %s type: %s\n <br>", $eid, $name,$start,$end,$description,$type);
-    }
-}
-
-
-$result->close();
-
-
-$conn->close();
+	$conn->close();
 ?>
