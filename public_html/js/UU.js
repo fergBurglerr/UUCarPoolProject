313c7fe04349e5202186ec8abf08fd3d64d82790
Joseph Trammel <jatrammel@gmail.com>
2015-04-20 19:40:37 -0500

    Added accordion display for announcements

--- public_html/js/UU.js
+++ public_html/js/UU.js
@@ -10,8 +10,16 @@ $(document).ready(function () {
 	$.post('announcements.php',
 	{
 		action:"get"
-	}, function(data){
-		$('#announce').html(data);
+	}, function(json){
+		$("#announce").html("<div id=\"Announcements\"></div>");
+		$.each(JSON.parse(json), function(idx, obj){
+			$("#Announcements").append("<h3>" + obj.Date + "</h3>");
+			$("#Announcements").append("<div id=\"announcement" + idx + "\"></div>");
+			$("#announcement" + idx).append("<p>" + obj.Content + "</p>");
+		});
+		$("#Announcements").accordion({
+			collapsible: true
+		});
 	});
     });
 });
