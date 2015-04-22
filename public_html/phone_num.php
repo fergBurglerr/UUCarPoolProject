1a61bb6c407c9b84cf0634a65f637ff6074a5db2
Thomas Scully <thomas1374@yahoo.com>
2015-04-21 22:35:56 -0500

    tesing delete of phone numbers

--- public_html/phone_num.php
+++ public_html/phone_num.php
@@ -91,7 +91,7 @@
 			$result2->fetch();
 			echo "$col1";
 			if ($col1 == 0 ){
-				$stmt = $conn->prepare("DELETE FROM (Phone) WHERE phone_number = ? LIMIT 1;");
+				$stmt = $conn->prepare("DELETE FROM Phone WHERE phone_number = ? LIMIT 1");
 				$stmt->bind_param('s', $number2);
 				if($stmt->execute()) {
 					echo "\n$number Number deleted from Phone table";
