05722211f7d4a03d1a0c61b554759e9e6efaf3cc
fergburglerr <ngfnbf@mail.missouri.edu>
2015-04-30 19:35:09 -0500

    updated registration form

--- public_html/js/UU.js
+++ public_html/js/UU.js
@@ -70,9 +70,61 @@ function loginForm(){
 }
 
 function registerForm(){
+	$.get('registerForm.php', {loggedin:"false"},
+	function(form){
+		$('#login').html(form);
+		$('#register').css('background-color', '#EEE');
 
+	});
 }
 
 function register(){
+	var username = $('#username').val();
+	var password = $('#password').val();
+	var checkPass=$('#password2').val();
+	var houseNumber = $('#houseNumber').val();
+	var suiteNumber = $('#suiteNumber').val();
+	var street = $('#street').val();
+	var city = $('#city').val();
+	var zip = $('#zip').val();
+	var email = $('#email').val();
+	var phone = $('#phone').val();
 
+	if(!(username.trim())){
+		alert("Username can't be blank");
+	} else if (!(password.trim())){
+		alert("Password can't be blank");
+	} else if (!(checkPass.trim())){
+		alert("Please retype password in second box!");
+	} else if (!(password == checkPass)){
+		alert("Passwords don't match!");
+	} else if (!(houseNumber.trim())){
+		alert("House number can't be blank!");
+	} else if (!(street.trim())){
+		alert("Street name can't be blank!");
+	} else if (!(city.trim())){
+		alert("City name can't be blank!");
+	} else if (!(zip.trim())){
+		alert("Zip code can't be blank!");
+	} else if (!(zip.length==5)){
+		alert("Zip code must be 5 chars long!");
+	} else if ((!(email)) && (!(phone))){
+		alert("An e-mail address or phone number is required!");
+	} else {
+		$.post('register.php',
+		{
+			username:username,
+			password:password,
+			houseNumber:houseNumber,
+			suiteNumber:suiteNumber,
+			street:street,
+			city:city,
+			zip:zip,
+			email:email,
+			phone:phone
+		},
+		function(data){
+			
+		});
+	}
 }
