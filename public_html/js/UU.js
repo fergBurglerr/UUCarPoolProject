bc214a5d2d089455a8d3d13db238e625c455c226
fergburglerr <ngfnbf@mail.missouri.edu>
2015-05-04 17:50:40 -0500

    Added getCookie function to js file

--- public_html/js/UU.js
+++ public_html/js/UU.js
@@ -140,3 +140,14 @@ function register(){
 		});
 	}
 }
+
+//getCookie function curtosey of w3 schools
+function getCookie(cname){
+	var name = cname + "=";
+	var ca = document.cookie.split(';');
+	for(var i=0; i<ca.length; i++){
+ 		var c = ca[i].trim();
+		if (c.indexOf(name)==0) return c.substring(name.length,c.length)
+	}
+	return "";
+}
