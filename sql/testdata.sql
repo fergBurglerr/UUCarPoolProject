12e8d761b9c8622eaa2271fea5b48cc3e1279211
JET005 <ethanwtsn.watson5@gmail.com>
2015-05-07 13:45:54 -0500

    added file for test data

---
+++ sql/testdata.sql
@@ -0,0 +1,21 @@
+INSERT INTO Address(houseNumber,suiteNumber,street,city,zipcode,latitude,longitude)
+	VALUES();
+
+
+
+INSERT INTO Person (firstName,lastName,emailAddress)
+VALUES();
+
+
+INSERT INTO `Group`(name,sponsor) VALUES();
+
+
+INSERT INTO Event(eventName,startTime,endTime,description,eventType,gid)
+	VALUES();
+
+INSERT INTO Announcement(content,aDate)
+	VALUES();
+
+INSERT INTO Phone VALUES ();
+
+INSERT INTO Email VALUES();
